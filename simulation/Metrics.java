c3889d6b5e019c6cb03423b0ceba942c2aeb982f
mohamadkhallaf <mohamad.khallaf@student.giu-uni.de>
2025-12-05 22:29:32 +0200

    metrics

--- simulation/Metrics.java
+++ simulation/Metrics.java
@@ -1,5 +1,52 @@
 package simulation;
 
 public class Metrics {
-    
+
+    // Per-process metrics
+    private int waitingTime;
+    private int turnaroundTime;
+    private int responseTime;
+    private int completionTime;
+
+    // Constructor
+    public Metrics(int waitingTime, int turnaroundTime, int responseTime, int completionTime) {
+        this.waitingTime = waitingTime;
+        this.turnaroundTime = turnaroundTime;
+        this.responseTime = responseTime;
+        this.completionTime = completionTime;
+    }
+
+    // Getters
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public int getResponseTime() {
+        return responseTime;
+    }
+
+    public int getCompletionTime() {
+        return completionTime;
+    }
+
+    // Setters (optional, if you want to update metrics later)
+    public void setWaitingTime(int waitingTime) {
+        this.waitingTime = waitingTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    public void setResponseTime(int responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public void setCompletionTime(int completionTime) {
+        this.completionTime = completionTime;
+    }
 }
